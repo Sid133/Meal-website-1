070d6a985ce2f299fc7d86c8fbc0b569730b349b
Sid133 <72150926+Sid133@users.noreply.github.com>
2020-10-25 13:14:56 +0530

    Add files via upload

---
+++ abc/abc.py
@@ -0,0 +1,19 @@
+import pyttsx3 #pip install pyttsx3
+import speech_recognition as sr #pip install speechRecognition
+import datetime
+import wikipedia #pip install wikipedia
+import webbrowser
+import os
+import smtplib
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    
+  speak("aryan")
